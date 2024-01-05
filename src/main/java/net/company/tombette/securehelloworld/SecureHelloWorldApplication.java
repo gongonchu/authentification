376ba452babf050fe2220f8b9c20786d190c7cf2
gtombette <gontran.tombette@soprasteria.com>
2024-01-05 16:59:02 +0100

    Initial commit

---
+++ src/main/java/net/company/tombette/securehelloworld/SecureHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package net.company.tombette.securehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/company/tombette/securehelloworld/SecureHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package net.company.tombette.securehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
